40449d4a217a19334eae32fe2c11471cdc772def
Angel Limas <angel.eliseo@gmail.com>
2019-03-03 04:11:32 -0600

    Jasimine implemented and test suite created

---
+++ JS_Files/Guessing_Logic.js
@@ -0,0 +1,10 @@
+/*
+We are building a guessing game that will have the following functionality:
+
+Program generates a random number at the start of each game
+User's goal is to guess the correct number
+User gets a set # of tries (default = 5)
+Program tracks user's guesses and prevents duplicate guessing
+Game ends when user guesses correctly or runs out of tries
+*/
+
