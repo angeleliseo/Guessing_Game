33b993652f9de9524641ad35f5e07ddeceace51e
Angel Limas <angel.eliseo@gmail.com>
2019-03-03 04:15:03 -0600

    missed untracked files added

---
+++ Tests/Tests4Game.js
@@ -0,0 +1,42 @@
+describe('Infrastructure validation', () => {
+    it('Canary Test', () => {
+        expect(true).toBeTruthy();
+    });
+});
+
+describe('Guessing game test suite', () => {
+    it('should fail if a string is returned', () => {
+        expect().nothing();
+    });
+    it('should be ok if a number is returned', () => {
+        expect().nothing();
+    });
+    it('same number should not be returned all the time', () => {
+        expect().nothing();
+    });
+    it('should start with a number', () => {
+        expect().nothing();
+    });
+    it('number of tries has to start at 5', () => {
+        expect().nothing();
+    });
+    it('tries cant be 0', () => {
+        expect().nothing();
+    });
+    it('trires cant be negative', () => {
+        expect().nothing();
+    });
+    it('stop guessing when no more tries', () => {
+        expect().nothing();
+    });
+    it('tries pool should decrease when tried', () => {
+        expect().nothing();
+    });
+    it('stop guessing when tell the right number', () => {
+        expect().nothing();
+    });
+    it('no duplicate guessing', () => {
+        expect().nothing();
+    });
+
+});
